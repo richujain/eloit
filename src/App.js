0d74507b212718e0e452b88371a7e1d5cd00dc2e
richujain <richujain1995@gmail.com>
2024-01-03 16:59:12 -0500

    About Us components added. Random data is fetched from an API and displayed successfully. Also added Media Queris to make it responsive.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Banner from "./components/Banner/Banner";
+import AboutUs from "./components/AboutUs/AboutUs";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
       <Banner />
+      <AboutUs />
       <p>Body</p>
       <p>Footer</p>
     </React.Fragment>
---
+++ src/components/AboutUs/AboutUs.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import classes from "./AboutUs.module.css";
+
+export default function AboutUs() {
+  const [employees, setEmployees] = useState([]);
+
+  const fetchEmployees = () => {
+    return fetch("https://randomuser.me/api/0.8/?results=4&nat=us")
+      .then((response) => {
+        return response.json();
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchEmployees().then((response) => {
+      const filteredEmployeeData = response.results.map((employee) => {
+        return {
+          fullName:
+            `${employee.user.name.first}` + ` ${employee.user.name.last}`,
+          image: employee.user.picture.large,
+          email: employee.user.email,
+          cell: employee.user.cell,
+        };
+      });
+      setEmployees(filteredEmployeeData);
+    });
+  }, []);
+
+  return (
+    <div className={classes.aboutUsContainer}>
+      <p className={classes.title}>About Us</p>
+      <div className={classes.listContainer}>
+        {employees.map((employee) => {
+          return (
+            <div
+              className={classes.individualEmployeeContainer}
+              key={employee.email}
+            >
+              <img
+                className={classes.employeeImage}
+                src={`${employee.image}`}
+                alt="Employee"
+              />
+              <p className={classes.employeeText}>{employee.fullName}</p>
+              <p className={classes.employeeText} style={{ fontWeight: "400" }}>
+                {employee.email}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
