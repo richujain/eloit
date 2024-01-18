54f6d8bb2e41ac52e65b43c959f5ac735bea2b5f
richujain <richujain1995@gmail.com>
2024-01-18 01:12:06 -0500

    Explore section completed

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -2,5 +2,40 @@ import React from "react";
 import classes from "./Explore.module.css";
 
 export default function Explore() {
-  return <div className={classes.exploreContainer}></div>;
+  return (
+    <div className={classes.exploreContainer}>
+      <p className={classes.title}>Explore</p>
+      <div className={classes.itemsContainer}>
+        <div className={classes.item}>
+          <p className={classes.heading}>Academics</p>
+          <p className={classes.subHeading}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eius...
+          </p>
+        </div>
+        <div className={classes.item}>
+          <p className={classes.heading}>Arts</p>
+          <p className={classes.subHeading}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eius...
+          </p>
+        </div>
+        <div className={classes.item}>
+          <p className={classes.heading}>Library</p>
+          <p className={classes.subHeading}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eius...
+          </p>
+        </div>
+        <div className={classes.item}>
+          <p className={classes.heading}>Sports</p>
+          <p className={classes.subHeading}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eius...
+          </p>
+        </div>
+      </div>
+      <button className={classes.button}>View More</button>
+    </div>
+  );
 }
